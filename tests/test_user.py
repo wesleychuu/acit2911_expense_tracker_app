ff1f6d82e926bfd0e32740e4a548ab73cd5cad4f
al-zivkovic <aldaman2@outlook.com>
2022-04-29 11:10:40 -0700

    Update test_user.py
    added some of the tests for the user class

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,1 +1,31 @@
 import pytest
+from models.user import User
+
+
+@pytest.fixture
+def new_user():
+    user1 = User(name="aleksandar zivkovic", email="azivkovic@my.bcit.ca",
+                 username="alziv", password="password123")
+    return user1
+
+
+def test_name():
+    assert new_user.name == "aleksandar zivkovic"
+
+
+def test_name_invalid():
+    """Name must be a string"""
+    with pytest.raises(ValueError):
+        User(name=12354, email="test@test.com",
+             username="hello", password="password")
+
+
+def test_email():
+    assert new_user.email == "azivkovic@my.bcit.ca"
+
+
+def test_email_invalid():
+    """Email must be a string"""
+    with pytest.raises(ValueError):
+        User(name="john smith", email=123123,
+             username="hello", password="password")
