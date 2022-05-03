5e83a080afaf4e5aaee48a7bd0936cbb1f787546
Kitty Laptop <kitty98low@gmail.com>
2022-05-02 20:39:11 -0700

    create functions for adding/deleting/editing user expenses

--- models/user.py
+++ models/user.py
@@ -1,4 +1,11 @@
+from cmath import exp
+import itertools
+from models.expense import Expense
+
+
 class User:
+
+    id_iter = itertools.count(start=0, step=1)
 
     def __init__(self, name, email, username, password):
         if type(name) is not str:
@@ -7,7 +14,32 @@ class User:
         if type(email) is not str:
             raise ValueError
 
+        self.id = next(User.id_iter)
         self.name = name
         self.email = email
         self.username = username
         self.password = password
+        self.expenses = []
+
+    def add_expense(self, expense: Expense):
+        """Add an expense to user's list"""
+        if type(expense) is not Expense:
+            raise TypeError
+
+        self.expenses.append(expense)
+
+    def delete_expense(self, expense: Expense):
+        """Remove an expense from user's list"""
+        index = None
+        for i, e in enumerate(self.expenses):
+            if e.get_expense_id() == expense.get_expense_id():
+                index = i
+                break
+
+        del self.expenses[index]
+
+    def edit_expense(self, expense_id: int, attr: str, value):
+        """Allow user to edit an existing expense"""
+        for e in self.expenses:
+            if e.get_expense_id() == expense_id:
+                e.edit_attr(attr, value)
