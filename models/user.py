6ff786d11493bb02e8e490fc3ca85196e31470f0
Kitty Laptop <kitty98low@gmail.com>
2022-05-02 21:25:21 -0700

    create to_dict function for User

--- models/user.py
+++ models/user.py
@@ -1,34 +1,37 @@
-import itertools
 from models.expense import Expense
 
 
 class User:
 
-    id_iter = itertools.count(start=0, step=1)
-
-    def __init__(self, name, email, username, password, expenses=[]):
+    def __init__(self, userid, name, username, email, password, expenses=[]):
         if type(name) is not str:
             raise ValueError
 
         if type(email) is not str:
             raise ValueError
 
-        self.id = next(User.id_iter)
+        self.id = userid
         self.name = name
-        self.email = email
         self.username = username
+        self.email = email
         self.password = password
         self.expenses = expenses
 
     def add_expense(self, expense: Expense):
-        """Add an expense to user's list"""
+        """Add an expense to user's list
+
+        Note: not quite right, need to fix
+        """
         if type(expense) is not Expense:
             raise TypeError
 
         self.expenses.append(expense)
 
     def delete_expense(self, expense: Expense):
-        """Remove an expense from user's list"""
+        """Remove an expense from user's list
+
+        Note: probably not right, need to fix
+        """
         index = None
         for i, e in enumerate(self.expenses):
             if e.get_expense_id() == expense.get_expense_id():
@@ -36,3 +39,17 @@ class User:
                 break
 
         del self.expenses[index]
+
+    def to_dict(self):
+        expenses = [e.to_dict() for e in self.expenses]
+
+        user_dict = {
+            "userid": self.id,
+            "name": self.name,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "expenses": expenses
+        }
+
+        return user_dict
--- models/user_manager.py
+++ models/user_manager.py
@@ -1,12 +1,22 @@
 import json
+import itertools
 
 from models.user import User
 
 
 class UserManager:
+
+    id_iter = itertools.count(start=0, step=1)
+
     def __init__(self):
-        self.users = []
         with open("data/users.json") as fp:
-            data = json.load(fp)
-            for user in data:
-                self.users.append(User(**user))
+            self.users = [
+                User(
+                    elem["userid"],
+                    elem["name"],
+                    elem["username"],
+                    elem["email"],
+                    elem["password"],
+                    elem["expenses"]
+                ) for elem in json.load(fp)
+            ]
