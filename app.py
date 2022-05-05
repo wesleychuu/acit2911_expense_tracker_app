b26708f6c43f1b81f77183ba46c4357fccc49cda
Kitty Laptop <kitty98low@gmail.com>
2022-05-05 13:02:45 -0700

    adjusted when connection obj creation is called

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request, redirect
 from modules.expense_module import *
 from modules.user_module import *
+from sql_db import create_connection
 from models.expense import CATEGORIES
 
 app = Flask(__name__)
@@ -19,7 +20,7 @@ def data_to_dict(data_tup: tuple) -> dict:
 @app.route("/user/<uid>")
 def homepage(uid):
     """Render the homepage of a user -- shows their expenses"""
-    conn = sqlite3.connect("database.db")
+    conn = create_connection("database.db")
     tuple_expenses = select_expenses_by_uid(conn, uid)
 
     total_category_exp = []
@@ -46,55 +47,44 @@ def add_expense(uid):
     """Adds an expense under the user's ID
     """
     data = request.form
+    conn = create_connection("database.db")
 
     try:
-        conn = sqlite3.connect("database.db")
         insert_expense(
             conn, uid, data["name"], data["date"], data["category"], data["amount"])
-        conn.close()
         return redirect(f'/user/{uid}'), 201
     except ValueError:
         return "", 400
+    finally:
+        conn.close()
 
 
 @app.route("/user/<uid>/edit/<eid>", methods=["GET"])
 def get_expense(uid, eid):
     """View an expense by user id and expense id for editing"""
+    conn = create_connection("database.db")
+
     try:
-        conn = sqlite3.connect("database.db")
         expense = select_one_expense(conn, eid=eid, uid=uid)
-        conn.close()
         return str(expense), 201
     except ValueError:
         return "", 400
+    finally:
+        conn.close()
 
 
 @app.route("/user/<uid>/edit/<eid>", methods=["DELETE"])
 def delete_expense(eid, uid):
     """Delete an expense by its id"""
+    conn = create_connection("database.db")
+
     try:
-        conn = sqlite3.connect("database.db")
         delete_one_expense(conn, eid, uid)
-        conn.close()
         return "", 201
     except ValueError:
         return "", 400
-
-
-# @app.route("/users", methods=["GET"])
-# def get_users():
-#     conn = sqlite3.connect("database.db")
-#     users = select_all_users(conn)
-#     conn.close()
-#     return str(users)
-
-
-# @app.route("/user/<uid>", methods=["GET"])
-# def get_user(uid):
-#     conn = sqlite3.connect("database.db")
-#     user = select_user_by_id(conn, uid)
-#     conn.close()
-#     return str(user)
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
