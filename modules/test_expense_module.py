68d35e4f9c60c66875acdac57022346d7f6fa9d4
al-zivkovic <aldaman2@outlook.com>
2022-05-05 15:33:57 -0700

    Update test_expense_module.py
    combined wesleys work with mine

--- modules/test_expense_module.py
+++ modules/test_expense_module.py
@@ -1,17 +1,43 @@
 from modules.expense_module import *
 import pytest
-import sql_db
+import sqlite3
 
 
-@pytest.fixture
+@pytest.fixture()
 def db():
-    conn = sql_db.create_connection('./database.db')
+    conn = sqlite3.connect("database.db")
     return conn
 
 
-def test_delete_one_expense(db):
-    d = delete_one_expense(db, 1, 1,)
-    pass
+def test_insert_expense(db):
+    """
+    Test if expenses can be added to database
+    """
+    assert insert_expense(db, 1, "Hot Dog", "2022-04-01", "Food", 1.50) == 5
+
+
+def test_select_one_expense(db):
+    assert select_one_expense(db, 5, 1) == (
+        5, 1, "Hot Dog", "2022-04-01", "Food", 1.5)
+
+
+def test_select_expenses_by_uid(db):
+    assert select_expenses_by_uid(db, 2) == [
+        (
+            4,
+            2,
+            "Burger",
+            "2022-03-29",
+            "Food",
+            10.95,
+        )
+    ]
+
+
+# def test_delete_one_expense(db):
+#     cur = db.cursor()
+#     delete_one_expense(db, 5, 1)
+#     cur.execute("SELECT length(* FROM expenses WHERE user_id=?)", (1,))
 
 
 def test_get_total_expenses(db):
