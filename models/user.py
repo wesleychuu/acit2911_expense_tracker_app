58bf4c063c796037dd0c8dcf8c6f6ed701d45c21
al-zivkovic <aldaman2@outlook.com>
2022-05-09 17:52:52 -0700

    completed user class
    made some minor changes to the test_user file.

--- models/user.py
+++ models/user.py
@@ -3,53 +3,69 @@ from models.expense import Expense
 
 class User:
 
-    def __init__(self, userid, name, username, email, password, expenses=[]):
+    def __init__(self, name, username, email, password):
         if type(name) is not str:
+            raise TypeError
+
+        if type(username) is not str:
+            raise TypeError
+
+        if len(username) < 5:
             raise ValueError
 
         if type(email) is not str:
+            raise TypeError
+
+        if "@" not in email:
             raise ValueError
 
-        self.id = userid
+        if type(password) is not str:
+            raise TypeError
+
+        if len(password) < 8:
+            raise ValueError
+
+        if any(chr.isdigit() for chr in password) == False:
+            raise ValueError
+
+        if any(chr.isalpha() for chr in password) == False:
+            raise ValueError
+
         self.name = name
         self.username = username
         self.email = email
         self.password = password
-        self.expenses = expenses
+    #     self.expenses = expenses
 
-    def add_expense(self, expense: Expense):
-        """Add an expense to user's list
+    # def add_expense(self, expense: Expense):
+    #     """Add an expense to user's list"""
+    #     if type(expense) is not Expense:
+    #         raise TypeError
 
-        Note: not quite right, need to fix
-        """
-        if type(expense) is not Expense:
-            raise TypeError
+    #     self.expenses.append(expense.to_dict())
 
-        self.expenses.append(expense)
+    # def delete_expense(self, expense: Expense):
+    #     """Remove an expense from user's list
 
-    def delete_expense(self, expense: Expense):
-        """Remove an expense from user's list
+    #     Note: probably not right, need to fix
+    #     """
+    #     index = None
+    #     for i, e in enumerate(self.expenses):
+    #         if e.get_expense_id() == expense.get_expense_id():
+    #             index = i
+    #             break
 
-        Note: probably not right, need to fix
-        """
-        index = None
-        for i, e in enumerate(self.expenses):
-            if e.get_expense_id() == expense.get_expense_id():
-                index = i
-                break
+    #     del self.expenses[index]
 
-        del self.expenses[index]
+    # def to_dict(self):
+    #     expenses = [e.to_dict() for e in self.expenses]
 
-    def to_dict(self):
-        expenses = [e.to_dict() for e in self.expenses]
+    #     user_dict = {
+    #         "name": self.name,
+    #         "username": self.username,
+    #         "email": self.email,
+    #         "password": self.password,
+    #         "expenses": expenses
+    #     }
 
-        user_dict = {
-            "userid": self.id,
-            "name": self.name,
-            "username": self.username,
-            "email": self.email,
-            "password": self.password,
-            "expenses": expenses
-        }
-
-        return user_dict
+    #     return user_dict
