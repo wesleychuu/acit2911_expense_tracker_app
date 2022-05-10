16c6e77035f8c5ac8502207a75357d8722889555
Kitty Laptop <kitty98low@gmail.com>
2022-05-10 15:40:45 -0700

    signup form validation and popup messages for errors

--- models/register_form.py
+++ models/register_form.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email
+from wtforms.validators import InputRequired, Length, Email, Regexp
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('name', validators=[
+    name = StringField('Name', validators=[
                        InputRequired(), Length(min=4, max=50)])
-    username = StringField('username', validators=[
+    username = StringField('Username', validators=[
                            InputRequired(), Length(min=4, max=30)])
-    email = StringField('email', validators=[InputRequired(), Email(
+    email = StringField('Email', validators=[InputRequired(), Email(
         message="Invalid email"), Length(max=50)])
-    password = PasswordField('password', validators=[
-                             InputRequired(), Length(min=8, max=80)])
+    password = PasswordField('Password', validators=[
+                             InputRequired(),
+                             Length(min=8, max=80),
+                             Regexp('(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)',
+                                    message="Passwords must be 8 characters long with at least one uppercase character, one lowercase character, and one special character.")
+                             ])
